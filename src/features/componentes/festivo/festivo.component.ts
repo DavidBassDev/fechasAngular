a51d8022f59f686f36564b7891fe274c692fd921
unknown <andres.a97@hotmail.com>
2025-05-30 18:06:53 -0500

    no changes

--- src/features/componentes/festivo/festivo.component.ts
+++ src/features/componentes/festivo/festivo.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { ReferenciasMaterialModule } from '../../../shared/modulos/referencias-material.module';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-festivo',
   imports: [
     ReferenciasMaterialModule,
+    FormsModule,
+    NgxDatatableModule
   ],
   templateUrl: './festivo.component.html',
   styleUrl: './festivo.component.css'
